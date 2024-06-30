4ca400120f809c25aef141108dcf61ae83dd28ea
Khalil Mouteirek <khalil.mouteirek@gmail.com>
2024-06-30 20:08:32 +0300

    added nextSequence function and fixed audio

--- Simon Says - HW/Scripts/game.js
+++ Simon Says - HW/Scripts/game.js
@@ -24,17 +24,35 @@ button.addEventListener("click", function() {
     let userChosenColor = this.id;
     userClickedPattern.push(userChosenColor);
     playSound(userChosenColor);
-    
+
     animatePress(userChosenColor);
     answer(userClickedPattern.length - 1);
 });
 });
 
 function nextSequence() {
+    userClickedPattern = [];
+    level++;
+    document.querySelector("#level-title").textContent = "Level " + level;
+    let randomNumber = Math.floor(Math.random() * 4);
+    let randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+    
+    setTimeout(function() {
+        document.getElementById(randomChosenColor).classList.add("pressed");
+        setTimeout(function() {
+        document.getElementById(randomChosenColor).classList.remove("pressed");
+        }, 100);
+        playSound(randomChosenColor);
+    }, 500);
 }
 
 function playSound() {
-
+    
+  let audio = document.getElementById(name + "Sound");
+  if (audio) {
+      audio.play();
+  }
 }
 
 function animatePress() {
