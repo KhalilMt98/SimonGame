0bda6912dd8f3693ea0da12bea5abc5bd1dafa1e
Khalil Mouteirek <khalil.mouteirek@gmail.com>
2024-06-30 19:40:06 +0300

    test commit

---
+++ Simon Says - HW/Scripts/game.js
@@ -0,0 +1,45 @@
+let buttons = [
+document.getElementById("green"),
+document.getElementById("red"),
+document.getElementById("yellow"),
+document.getElementById("blue")
+];
+
+const buttonColors = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+let userClickedPattern = [];
+let started = false;
+let level = 0;
+
+document.addEventListener('keypress', function() {
+if (!started) {
+    document.querySelector("#level-title").textContent = "Level " + level;
+    nextSequence();
+    started = true;
+}
+});
+
+buttons.forEach(function(button) {
+button.addEventListener("click", function() {
+    let userChosenColor = this.id;
+    userClickedPattern.push(userChosenColor);
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+    answer(userClickedPattern.length - 1);
+});
+});
+
+function nextSequence() {
+}
+
+function playSound() {
+
+}
+
+function animatePress() {
+}
+
+function answer(currentLevel) {
+}
+
+
